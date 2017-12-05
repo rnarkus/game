36e9237a951bd0ff0416996587c4a027773a75b1
Markus <markus_hudobnik@hotmail.com>
2017-12-05 15:04:32 -0700

    added another library to help with responsive design, 'npm install react-responsive --save' Site should now fully be responsive and align with the page at all widths

--- src/Main.js
+++ src/Main.js
@@ -4,16 +4,29 @@ import Couch from "./couch";
 //import Contact from "./Contact";
 import Sidebar from "./sidebar";
 import {Flex, Box} from 'reflexbox';
+import Responsive from 'react-responsive';
 
+const Mobile = props => <Responsive {...props} maxWidth={767} />;
+const Default = props => <Responsive {...props} minWidth={768} />;
 
 class Main extends Component {
   render() {
     return (
-        <Flex justify='space-between' align='center' style={{ 'margin-top': '10%' }}>
-            <Box auto><Couch /></Box>
-            <Box auto><Sidebar /></Box>
+        <div>
+        <Mobile>
+          <Flex column justify='space-between' align='center' style={{ 'margin': '10%' }}>
+              <Box auto><Couch /></Box>
+              <Box auto><Sidebar /></Box>
+          </Flex>
+      </Mobile>
+        <Default>
+          <Flex justify='space-between' align='center' style={{ 'margin': '10%' }}>
+              <Box auto><Couch /></Box>
+              <Box auto><Sidebar /></Box>
+          </Flex>
+      </Default>
+  </div>
 
-        </Flex>
 
 
     );
